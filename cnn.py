0faadc11a3aaedbe7ae2ac874326cb061ba2f27d
RATED-R-SUNDRAM <ss6437p@gmail.com>
2024-05-19 22:23:25 +0530

    cnn complete model with 82% acccuracy

--- cnn.py
+++ cnn.py
@@ -1,4 +1,5 @@
 #%%
+from tkinter.ttk import _Padding
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -9,6 +10,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.utils
 import torchvision
+import torch.nn.functional as F
 #%%
 import torch
 print(torch.cuda.is_available())
@@ -33,3 +35,59 @@ plt.imshow(images[0].squeeze())
 print(labels[0])
 # %%
 
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(1,6,3)
+        self.maxpool = nn.MaxPool2d(2)
+        self.conv2 = nn.Conv2d(6, 16, 3)
+        self.fc1 = nn.Linear(16*25, 128)
+        self.fc2 = nn.Linear(128, 10)
+        self.conv3 = nn.Linear(10,1,)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))# (28-3)+1 (26,26)
+        x = self.maxpool(x) # (26//2) (13,13)
+        x = F.relu(self.conv2(x)) # (11,11)
+        x = self.maxpool(x)#(5,5)
+        x = x.reshape(x.shape[0], -1) # (16*5*5)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        return x
+    
+
+model = CNN()
+optimizer= torch.optim.SGD(model.parameters(),lr=0.001)
+loss= nn.CrossEntropyLoss()
+lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.9)
+
+# %%
+
+epoch=0 
+
+while(epoch <=20):
+    for i,(images,labels) in enumerate(train_data):
+        output = model(images)
+        loss_value = loss(output,labels)
+        optimizer.zero_grad()
+        loss_value.backward()
+        optimizer.step()
+        if(i%100==0):
+            print(f"Epoch {epoch} Batch {i} Loss {loss_value.item()}")
+    lr_scheduler.step()
+    epoch+=1
+
+# %%
+
+tot=0
+cnt=0
+with torch.no_grad():
+    for i,(images,labels) in enumerate(test_data):
+        output = model(images)
+        _,predicted = torch.max(output,1)
+        tot+=64
+        cnt+= (predicted==labels).sum().item()
+print(cnt/tot)
+
+        
+# %%
